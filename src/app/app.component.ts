d925b5c3c288e97d8b018b35205fd9a94a29225a
Mario Mora SRT <mariomora.srt@gmail.com>
2019-01-22 11:19:33 -0800

    Adding first version of full app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'basicCalc';
+	title = 'CalcU';
+
+	num1:number;
+	num2:number;
+
+	symbol:string;
+
+	result:string;
+
+
+//{{num1}} {{symbol}} {{num2}} = {{result}}	
+	calc(opt){
+		let value = this.operation(opt)
+		this.result = this.num1 + ' ' + this.symbol + ' ' + this.num2 + ' = ' + value;
+	}
+
+	operation(opt){
+		switch(opt){
+			case 1: // sum
+				this.symbol = '+';
+				return +this.num1 + +this.num2;
+			break;
+			case 2: // substration
+				this.symbol = '-';
+				return this.num1 - this.num2;
+			break;
+			case 3: // multiplication
+				this.symbol = '*';
+				return this.num1 * this.num2;
+			break;
+			case 4: // division
+				this.symbol = '/';
+				return this.num1 / this.num2;
+			break;
+
+		}
+	}
+
+
 }
